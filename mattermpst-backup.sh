e0e435109287588ddfdfaca11ba23ad3efe147a6
3mdf1v3 <3mdf1v3@gmail.com>
2020-11-27 09:43:48 +0100

    Create mattermpst-backup.sh

---
+++ mattermpst-backup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+DATE=$(date +%Y-%m-%d-%H%M%S)
+DESTINATION='./backup'
+FILETOBACKUP='./system-file-backup.txt'
+MYSQLFILE="${DESTINATION}/mariadb-${DATE}.sql"
+CONTAINERIP=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' mariadb)
+
+mysqldump -u root -p${MYSQLPWD} -h ${CONTAINERIP} --all-databases --lock-tables=0 > ${MYSQLFILE}
+
+echo './mattermost/data/' >> ${FILETOBACKUP}
+echo './mattermost/config/' >> ${FILETOBACKUP}
+echo ${MYSQLFILE} >> ${FILETOBACKUP}
+
+find ${DESTINATION} -type f -mtime +7 -exec rm -f {} \;
+
+tar -czpf "${DESTINATION}/backup-${DATE}.tar.gz" -T ${FILETOBACKUP}
+rm -f ${MYSQLFILE} ${FILETOBACKUP}
+                                   
