f57a77944d3a71cb372175c97ef60b1c290843bb
3mdf1v3 <3mdf1v3@gmail.com>
2021-01-25 16:10:49 +0100

    Aggiunto bot mattermost per controllo files creati

--- mattermpst_backup.sh
+++ mattermpst_backup.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 DATE=$(date +%Y-%m-%d-%H%M%S)
-DESTINATION='./backup'
-FILETOBACKUP='./system-file-backup.txt'
+DESTINATION='/root/mattermost-backups'
+BACKUPFILES="${DESTINATION}/mattermost-backup-${DATE}.list"
 MYSQLFILE="${DESTINATION}/mariadb-${DATE}.sql"
-CONTAINERIP=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' mariadb)
 
-mysqldump -u dbbackup -h ${CONTAINERIP} --all-databases --lock-tables=0 > ${MYSQLFILE}
+mysqldump -u root --all-databases > ${MYSQLFILE}
 
-echo './mattermost/data/' >> ${FILETOBACKUP}
-echo './mattermost/config/' >> ${FILETOBACKUP}
-echo ${MYSQLFILE} >> ${FILETOBACKUP}
+find ${DESTINATION} -name '*.tar.gz' -type f -mtime +7 -exec rm -f {} \;
 
-find ${DESTINATION} -type f -mtime +7 -exec rm -f {} \;
+echo '/opt/mattermost/config/config.json' >> ${BACKUPFILES}
+echo '/opt/mattermost/data/' >> ${BACKUPFILES}
 
-tar -czpf "${DESTINATION}/backup-${DATE}.tar.gz" -T ${FILETOBACKUP}
-rm -f ${MYSQLFILE} ${FILETOBACKUP}
-                                   
+echo ${MYSQLFILE} >> ${BACKUPFILES}
+
+tar -czpf "${DESTINATION}/mattermost-backups-${DATE}.tar.gz" -T ${BACKUPFILES}
+
+rm -f ${DESTINATION}/*.sql ${DESTINATION}/*.list
+
+A="$(ls -als /root/mattermost-backups/*.tar.gz)"
+B="{\"text\":\"#### Controllo backup mattermost del ${DATE}:\n<!channel> controllate la lista completa dei backup eseguiti.\n\`\`\` bash\n${A}\n\`\`\`\"}"
+curl -i -k -X POST -H 'Content-Type: application/json' -d "${B}" ${MATTERMOSTBOTURI}
